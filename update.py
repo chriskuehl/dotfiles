d5df6bad946ce616b5c6ca40266ebd3cfb7287df
Chris Kuehl <chris@techxonline.net>
2013-09-14 09:52:59 -0700

    Check out modules when updating.

--- update.py
+++ update.py
@@ -2,6 +2,11 @@
 import glob
 import os
 
+os.system("git submodule init")
+print("git submodule init")
+os.system("git submodule update")
+print("git submodule update")
+
 for file in glob.glob("*"):
 	if not(file.endswith(".py")):
 		cmd = "ln -s {} ~/.{}".format(os.path.abspath(file), file)
