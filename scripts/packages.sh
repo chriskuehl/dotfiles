b47a18d4ea50b8d481936606e97b8b7a8060ae82
Chris Kuehl <ckuehl@ckuehl.me>
2022-11-11 00:09:32 -0600

    Update packages to install

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get update
-apt-get install -y zsh vim tmux git htop mtr tcpdump python-pip python-dev \
-	python3 python3-pip python3-dev iftop pv rsync curl screen pwgen \
+apt-get install -y zsh vim tmux git htop mtr tcpdump \
+	python3 python3-dev iftop iotop pv rsync curl screen pwgen \
 	zip unzip
