c524ff1542e629c3b83a25916be99720c50d45da
Chris Kuehl <chris.kuehl@reddit.com>
2024-11-21 10:43:06 -0600

    Add tabline

--- nvim-init.lua
+++ nvim-init.lua
@@ -97,6 +97,48 @@ require("lualine").setup({
             },
         },
     },
+    tabline = {
+        lualine_a = {
+            {
+                'buffers',
+                show_filename_only = true,   -- Shows shortened relative path when set to false.
+                hide_filename_extension = false,   -- Hide filename extension when set to true.
+                show_modified_status = true, -- Shows indicator when the buffer is modified.
+
+                mode = 0, -- 0: Shows buffer name
+                -- 1: Shows buffer index
+                -- 2: Shows buffer name + buffer index
+                -- 3: Shows buffer number
+                -- 4: Shows buffer name + buffer number
+
+                max_length = vim.o.columns * 2 / 3, -- Maximum width of buffers component,
+                -- it can also be a function that returns
+                -- the value of `max_length` dynamically.
+                filetype_names = {
+                    TelescopePrompt = 'Telescope',
+                    dashboard = 'Dashboard',
+                    packer = 'Packer',
+                    fzf = 'FZF',
+                    alpha = 'Alpha',
+                }, -- Shows specific buffer name for that filetype ( { `filetype` = `buffer_name`, ... } )
+
+                -- Automatically updates active buffer color to match color of other components (will be overidden if buffers_color is set)
+                use_mode_colors = true,
+
+                buffers_color = {
+                    -- Same values as the general color option can be used here.
+                    -- active = 'lualine_{section}_normal',     -- Color for active buffer.
+                    -- inactive = 'lualine_{section}_inactive', -- Color for inactive buffer.
+                },
+
+                symbols = {
+                    modified = ' ●',      -- Text to show when the buffer is modified
+                    alternate_file = '#', -- Text to show to identify the alternate file
+                    directory =  '',     -- Text to show when the buffer is a directory
+                },
+            }
+        },
+    },
 })
 
 -- Disable mouse.
@@ -233,7 +275,7 @@ lspconfig.gopls.setup({
     },
 })
 
-lspconfig.ruff.setup({})
+-- lspconfig.ruff.setup({})
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
