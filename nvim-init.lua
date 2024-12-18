eceb13fd6e19b9731faef989b424654bce9b55e2
Chris Kuehl <chris.kuehl@reddit.com>
2024-12-17 18:52:58 -0600

    Increase max_length of tab line

--- nvim-init.lua
+++ nvim-init.lua
@@ -124,7 +124,7 @@ require("lualine").setup({
                 -- 3: Shows buffer number
                 -- 4: Shows buffer name + buffer number
 
-                max_length = vim.o.columns * 2 / 3, -- Maximum width of buffers component,
+                max_length = vim.o.columns, -- Maximum width of buffers component,
                 -- it can also be a function that returns
                 -- the value of `max_length` dynamically.
                 filetype_names = {
