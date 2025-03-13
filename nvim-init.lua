379c1b2f7acd872583b6043be1cd27c33984dfc6
Chris Kuehl <chris.kuehl@reddit.com>
2025-03-13 09:49:22 -0500

    Disable editorconfig

--- nvim-init.lua
+++ nvim-init.lua
@@ -174,6 +174,7 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
+vim.g.editorconfig = false
 
 -- Faster updatetime for async vim-signify.
 vim.opt.updatetime = 100
