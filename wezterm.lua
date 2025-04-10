d1c86ea1d797aa1e41bdef8c29ffd478f0f05bc1
Chris Kuehl <chris.kuehl@reddit.com>
2025-04-10 10:45:15 -0500

    More wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -1,15 +1,24 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 config.check_for_updates = false
+
+local default_font_size = 14
+if wezterm.hostname() == 'overlook' then
+    default_font_size = 22
+end
+
 config.window_frame = {
-    font_size = 22,
+    font_size = default_font_size,
 }
 config.bold_brightens_ansi_colors = true
 config.font = wezterm.font(
+    -- https://fonts.google.com/noto/specimen/Noto+Sans+Mono
     'Noto Sans Mono',
     { weight = 'Bold' }
 )
 config.colors = {
+    cursor_bg = 'rgb(255 150 244)',
+    cursor_fg = 'black',
     foreground = 'white',
     background = '#2aaa2aaa2aaa',
     ansi = {
